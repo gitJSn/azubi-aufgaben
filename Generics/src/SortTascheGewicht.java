db3113c00ed66d83a024d89177c15f6878dca066
steffen.jaser <steffen.jaser@aracom.de>
2022-10-27 16:48:16 +0200

    removed generics folder

--- Generics/src/SortTascheGewicht.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class SortTascheGewicht implements Comparator<Bag<?, ? extends Number>> {
-    @Override
-    public int compare(Bag o1, Bag o2) {
-        return Double.compare(o1.getWeight().doubleValue(), o2.getWeight().doubleValue());
-    }
-}
